d1a15e2dac1e6c0bf193d5e3b22403c69ae9ca64
Rokas <rookas.rudzenskas@gmail.com>
2023-07-10 10:42:15 +0300

    ROKAS

---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+"use client";
+import React from 'react';
+import {} from 'next/image';
+
+const Header = ({}) => {
+  return (
+    <header clasName="">
+      <Image
+        src="https://links.papareact.com/208"
+        alt="Logo"
+        width={200}
+        height={200}
+      />
+    </header>
+  );
+
+
+export default Header;
+// by Rokas with ❤️
